1798123499acb1c3fcc84820126ed474fbb650fd
cel71 <celestialc71@libero.it>
2021-06-29 15:08:37 +0200

    vue-todolist es1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+new Vue ({
+    el:"#app",
+    data: {
+        toDo: [
+            "Fare i compiti",
+            "Fare la spesa",
+            "Fare il bucato",
+        ],
+        elementList: "",
+    },
+    methods: {
+        aggiungi: function() {
+            this.toDo.push(this.elementList);
+            this.elementList = "";
+        },
+        cancella: function(index) {
+            this.toDo.splice(index, 1);
+        }
+    },
+})
